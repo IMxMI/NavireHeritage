adb751e2189515950fe23446dfc29f3ea77a09ea
IIMxMII <mxm.vernoux@gmail.com>
2023-03-08 10:49:29 +0100

    Correction erreur

--- NavireHeritage/ClassesMetier/Cargo.cs
+++ NavireHeritage/ClassesMetier/Cargo.cs
@@ -6,17 +6,20 @@ using System.Threading.Tasks;
 
 namespace NavireHeritage.ClassesMetier
 {
-    class Cargo : Navire
+    class Cargo : Navire, Station.Interface.INavCommercable
     {
         private string typeFret;
 
-        public Cargo(string imo, string nom, string latitude, string longitude, int tonnageDT, int tonnageDWT, int tonnageActuel, string typeFret) :base(imo, nom, latitude, longitude, tonnageDT, tonnageDWT, tonnageActuel)
+        public Cargo(string imo, string nom, string latitude, string longitude, int tonnageGT, int tonnageDWT, int tonnageActuel, string typeFret) :base(imo, nom, latitude, longitude, tonnageGT, tonnageDWT, tonnageActuel)
         {
             this.typeFret = typeFret;
         }
 
         public string TypeFret { get => typeFret;}
 
-        public 
+        public void Charger(int qte)
+        {
+            
+        }
     }
 }
