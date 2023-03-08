48fb461f6828019d521b2a32b4d0c7ba2159aa04
IIMxMII <mxm.vernoux@gmail.com>
2023-03-08 10:39:27 +0100

    methode

--- NavireHeritage/Interfaces/IStationable.cs
+++ NavireHeritage/Interfaces/IStationable.cs
@@ -8,5 +8,15 @@ namespace Station.Interface
 {
     interface IStationable
     {
+        void EnregistrerArriveePrevue(Object objects);
+        void EnregistrerArrivee(String objects);
+        void EnregistrerDepart(String objects);
+        bool EstAttendu(String id);
+        bool EstPresent(String id);
+        bool EstParti(String id);
+        object GetUnAttendu(String id);
+        object GetUnArrive(String id);
+        object GetUnParti(String id);
+
     }
 }
