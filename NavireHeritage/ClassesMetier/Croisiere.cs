fcb22dba89d62ed1b4c9a913bdb1c8e4c83d6a7d
sebastien crochard <sebastien1.crochard@outlook.fr>
2023-03-15 09:13:33 +0100

    classe passager et croisière

--- NavireHeritage/ClassesMetier/Croisiere.cs
+++ NavireHeritage/ClassesMetier/Croisiere.cs
@@ -6,7 +6,56 @@ using System.Threading.Tasks;
 
 namespace NavireHeritage.ClassesMetier
 {
-    class Croisiere
+    class Croisiere : Navire
     {
-    }
+        private char typeNavireCroisiere;
+        private int nbPassagersMaxi;
+        private Dictionary<String, Passager> passagers;
+
+		public char TypeNavireCroisiere { get => typeNavireCroisiere;}
+		public int NbPassagersMaxi { get => nbPassagersMaxi;}
+		internal Dictionary<string, Passager> Passagers { get => passagers;}
+
+		public Croisiere(string imo, string nom, double latitude, double longitude, int tonnageGT, int tonnageDWT, int tonnageActuel, char typeNavireCroisiere, int nbPassagersMaxi) : base( imo,  nom,  latitude,  longitude,  tonnageGT,  tonnageDWT,  tonnageActuel)
+		{
+			this.typeNavireCroisiere = typeNavireCroisiere;
+			this.nbPassagersMaxi = nbPassagersMaxi;
+		}
+
+		public Croisiere(string imo, string nom, double latitude, double longitude, int tonnageGT, int tonnageDWT, int tonnageActuel, char typeNavireCroisiere, int nbPassagersMaxi, List<Passager> passagers) : base(imo, nom, latitude, longitude, tonnageGT, tonnageDWT, tonnageActuel)
+		{
+			passagers = new List<Passager>();
+		}
+		public void Embarquer(List<Passager> passagers)
+		{
+			if (passagers.Count < nbPassagersMaxi)
+			{
+				foreach(Passager passager1 in passagers)
+				{
+					passagers.Add(passager1);
+				}
+			}
+			else
+			{
+				throw new Exception("Erreur le navire est déjà plein");
+			}
+		}
+		public List<Passager> Debarquer(List<Passager> passagers)
+		{
+			int count = 0;
+			if(passagers.Count > 0)
+			{
+				foreach(Passager passager1 in this.passagers.Values)
+				{
+					passagers.RemoveAt(count);
+					count++;
+				}
+			}
+			else
+			{
+				throw new Exception("Erreur le bateau est déjà vide");
+			}
+			return passagers;
+		}
+	}
 }
