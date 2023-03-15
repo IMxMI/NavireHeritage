3663a7b50cad0297b48d4a69c6dcea7e6235378a
IIMxMII <mxm.vernoux@gmail.com>
2023-03-15 09:42:05 +0100

    Commentaire

--- NavireHeritage/Interfaces/IStationable.cs
+++ NavireHeritage/Interfaces/IStationable.cs
@@ -8,14 +8,35 @@ namespace Station.Interface
 {
     interface IStationable
     {
+        /// <summary>
+        /// Méthode qui met à jour la liste des objets qui sont
+        /// susceptibles d'arriver dans la station (port, aéroport,…).
+        /// </summary>
+        /// <param name="objects"></param>
         void EnregistrerArriveePrevue(Object objects);
+
+        /// <summary>
+        /// Méthode qui enregistre l'arrivée réelle de l'objet. 
+        /// </summary>
+        /// <param name="objects"></param>
         void EnregistrerArrivee(String objects);
+
+        /// <summary>
+        /// Méthode qui enregistre le départ d'un objet présent dans la station.
+        /// </summary>
+        /// <param name="objects"></param>
         void EnregistrerDepart(String objects);
+
         bool EstAttendu(String id);
+
         bool EstPresent(String id);
+
         bool EstParti(String id);
+
         object GetUnAttendu(String id);
+
         object GetUnArrive(String id);
+
         object GetUnParti(String id);
 
     }
