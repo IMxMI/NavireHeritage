59f8d163087314da360997e670757596dc3ed399
IIMxMII <mxm.vernoux@gmail.com>
2023-03-22 10:58:19 +0100

    Test

--- NavireHeritage/Progamme/Program.cs
+++ NavireHeritage/Progamme/Program.cs
@@ -18,9 +18,17 @@ namespace NavireHeritage
                 Port port = new Port("Marseille", "43.2976N", "5.3471E", 4, 3, 2, 4);
                 Test.ChargementInitial(port);
                 Console.WriteLine(port);
-                Test.AfficheAttendus(port);
-                Test.TestEnregistrerArriveePrevue(port, new Cargo("IMO9780859", "CMA CGM A. LINCOLN", 43.43279, 134.76258,
-                    140872, 148992, 123000, "marchandises diverses"));
+
+
+
+
+                //Test.AfficheAttendus(port);
+                //Test.TestEnregistrerArriveePrevue(port, new Cargo("IMO9780859", "CMA CGM A. LINCOLN", 43.43279, 134.76258,
+                //    140872, 148992, 123000, "marchandises diverses"));
+                //Test.TestEnregistrerArriveePrevue(port, new Cargo("IMO9788889", "LINCOLN", 43.47279, 134.77258,
+                //   140852, 148962, 123080, "marchandises diverses"));
+                //Test.AfficheAttendus(port);
+
 
                 // throw new GestionPortException("Test création d'erreur");
 
